d9922d5a8e4e17a8bcb1ad858a3e7720bd7bd0d6
Nuro <82734446+NuroC@users.noreply.github.com>
2022-07-19 08:44:36 -0400

    Update ttv emote script.js

--- ttv emote script.js
+++ ttv emote script.js
@@ -1,12 +1,9 @@
 // ==UserScript==
 // @name         ttv emote script
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      1
+// @description  automatically play the game for you
+// @author       Nuro
 // @match        *://emote-racer.herokuapp.com/*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=herokuapp.com
-// @grant        none
 // @run-at       document-start
 // ==/UserScript==
 
